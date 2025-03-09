758eadca70651769d5529ee84dab7413c289f79b
Tirtha Jyoti Bol <tirthabol555@gmail.com>
2025-03-09 15:45:09 +0530

    connection change

--- src/database/mongo.connection.js
+++ src/database/mongo.connection.js
@@ -5,7 +5,10 @@ dotenv.config();
 export async function connect(){
     try{
         let mongo_uri = process.env.mongo_url;
-        mongoose.connect(mongo_uri);
+        mongoose.connect(mongo_uri).then(()=>{
+            console.log('connected');
+        }).catch((err)=>console.log(err)
+        )
         console.log('connected to mongo db');
     }
     catch(err){
